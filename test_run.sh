8d8aecf5e486fe863187144ccf1653967177bffe
Sean Sullivan <afidclan@gmail.com>
2025-10-07 22:29:54 -0700

    Working?

--- test_run.sh
+++ test_run.sh
@@ -2,6 +2,6 @@ cp base-case/ run/ -r
 cd run/
 surfaceFeatureExtract
 blockMesh
-snappyHexMesh
-decomposePar -force
-mpirun -np 4 simpleFoam -parallel
+snappyHexMesh -overwrite
+-decomposePar -force
+-mpirun -np 4 simpleFoam -parallel
